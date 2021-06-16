b845a987991d0dec489a87d104f88e809b75457b
Tanguy Cavagna <tanguy.cvgn@gmail.com>
2021-06-16 13:09:52 +0200

    Added authentication and resolve server/client mismatch

---
+++ packages/client/theme.ts
@@ -0,0 +1,21 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+  },
+});
+
+export default theme;
