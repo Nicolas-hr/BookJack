a9a1a0ea2dbce5dd4cd9728fc364902f5d5a08d6
Tanguy Cavagna <tanguy.cvgn@gmail.com>
2021-07-16 16:18:41 +0200

    Removed unnecessary import

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useAuth } from "react-use-auth";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core";
 import Navbar from "../components/Navbar";
 import { useUser } from "../custom-hooks/useUser";
