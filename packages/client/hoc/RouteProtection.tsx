c667d99de0527cbf1d291000e71f5178e172e496
Tanguy Cavagna <tanguy.cvgn@gmail.com>
2021-07-18 15:36:37 +0200

    Fixed perma reload of the landing page after logout

--- packages/client/hoc/RouteProtection.tsx
+++ packages/client/hoc/RouteProtection.tsx
@@ -1,38 +1,14 @@
 import React from "react";
 import { useAuth } from "react-use-auth";
+import Error from "next/error";
 
-/**
- * Force the user to be logged to see the given content
- *
- * @param Component Component to render after middleware passed
- * @returns
- */
-export const asLoggedRequired =
-  (Component: React.FunctionComponent) => (props: {}) => {
-    const { isAuthenticated } = useAuth();
+export const withProtect = (Component: React.FC) => (props: {}) => {
+  const { isAuthenticated } = useAuth();
 
-    if (isAuthenticated()) {
-      return <Component {...props} />;
-    } else {
-      window.location.replace("/");
-      return <React.Fragment></React.Fragment>;
-    }
-  };
+  if (!isAuthenticated()) {
+    window.location.replace(window.location.origin);
+    return <Error statusCode={401} title="Not authorized" />;
+  }
 
-/**
- * Force the user to NOT be logged (as lambda user) to see the given content
- *
- * @param Component Component to render after middleware passed
- * @returns
- */
-export const asLambdaRequired =
-  (Component: React.FunctionComponent) => (props: {}) => {
-    const { isAuthenticated } = useAuth();
-
-    if (isAuthenticated()) {
-      window.location.replace("/");
-      return <React.Fragment></React.Fragment>;
-    } else {
-      return <Component {...props} />;
-    }
-  };
+  return <Component {...props} />;
+};
